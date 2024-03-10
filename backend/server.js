f8d0a574b961af8956dd5795127688a682ff10cd
Anis-Naouali <anis.naouali@esprit.tn>
2024-03-10 01:21:16 +0100

    backend

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
+
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes); //
+app.use("/api/messages", messageRoutes);
+app.use("/api/user", userRoutes);
+
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`server running on port ${PORT}`);
+});
+
+
+//65ea78fa819c1163bdd2abe1
